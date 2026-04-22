30163255594b4ba5b6d858585f78b4e6f658d00a
bila8619 <shopifytest8615@gmail.com>
2026-04-23 04:45:35 +0500

    Update page.tsx

--- app/auth/sign-up/page.tsx
+++ app/auth/sign-up/page.tsx
@@ -53,7 +53,7 @@ export default function SignUpPage() {
         email,
         password,
         options: {
-          emailRedirectTo: process.env.NEXT_PUBLIC_DEV_SUPABASE_REDIRECT_URL || `${window.location.origin}/auth/login`,
+          emailRedirectTo: `https://plannerflow.shop/auth/confirm?next=/dashboard`,
         },
       })
       if (error) throw error
